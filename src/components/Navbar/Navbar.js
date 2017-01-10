3ff7cf7de86c3a4dae5fdb5a772dbf8496168fd8
Andy Mantell <andymantell@hotmail.com>
2017-01-10 12:43:55 +0000

    Add navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <span className="navbar-brand">ReactJS Trello clone</span>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
